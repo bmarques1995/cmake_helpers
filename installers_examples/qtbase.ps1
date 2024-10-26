dbe624e1c499d62050bc20702984488f22deb35e
Bruno S Marques <brunosmarques1995@hotmail.com>
2024-10-25 22:44:36 -0300

    Improved qt example

--- installers_examples/qtbase.ps1
+++ installers_examples/qtbase.ps1
@@ -1,11 +1,30 @@
 param (
     [string]$buildMode = "Debug",
     [string]$installPrefix,
-    [string]$moduleDestination
+    [string]$moduleDestination,
+    [string]$vsCompilerLocation
 )
 
-if (($buildMode -eq "Debug" -or $buildMode -eq "Release") -and ($installPrefix -ne "") -and ($moduleDestination -ne ""))
+function AppendVSNinjaHost {
+    param (
+        [string]$compilerLocation
+    )
+
+    $vsVersionRegex = "(.*Community|Enterprise|Professional)"
+    $vsBasePath = ""
+
+    if ($compilerLocation -match $vsVersionRegex) {
+        $vsBasePath = $matches[1]
+    } else {
+        Write-Output "Version not found"
+    }
+
+    $env:PATH = "$vsBasePath/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/Ninja;$env:PATH"
+}
+
+if (($buildMode -eq "Debug" -or $buildMode -eq "Release") -and ($installPrefix -ne "") -and ($moduleDestination -ne "") -and ($vsCompilerLocation -ne ""))
 {
+    AppendVSNinjaHost($vsCompilerLocation)
     git clone --recursive https://code.qt.io/qt/qtbase.git "$moduleDestination/modules/qtbase"
     cmake -S "$moduleDestination/modules/qtbase" -B "$moduleDestination/dependencies/windows/qtbase" -G "Ninja" -DCMAKE_INSTALL_PREFIX="$installPrefix" -DCMAKE_C_COMPILER=cl.exe -DCMAKE_CXX_COMPILER=cl.exe -DCMAKE_BUILD_TYPE="$buildMode" -DCMAKE_ASM_COMPILER=ml64.exe
     cmake --build "$moduleDestination/dependencies/windows/qtbase" --parallel --fresh
