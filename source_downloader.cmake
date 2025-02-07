2260780220c00067df59bf0757eaf3ce2188d640
bmarques1995 <testebash@outlook.com>
2025-02-06 22:52:39 -0300

    Added binary downloader

--- source_downloader.cmake
+++ source_downloader.cmake
@@ -40,3 +40,34 @@ macro(download_sources_from_git_repo)
     endforeach()
 
 endmacro()
+
+macro(download_remote_files)
+    set(oneValueArgs "SOURCE_BASE_OUTPUT_DIR")
+    set(multiValueArgs "SOURCE_INPUTS")
+    set(options "FORCE_DOWNLOAD")
+
+    cmake_parse_arguments(PACKAGE "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+    if((NOT DEFINED PACKAGE_SOURCE_INPUTS) OR
+       (NOT DEFINED PACKAGE_SOURCE_BASE_OUTPUT_DIR))
+        message(FATAL_ERROR "SOURCE_INPUTS, SOURCE_BASE_OUTPUT_DIR are required arguments")
+    endif()
+
+    if((NOT DEFINED PACKAGE_FORCE_DOWNLOAD))
+		set(PACKAGE_FORCE_DOWNLOAD OFF)
+    endif()
+
+    foreach(SOURCE_INPUT ${PACKAGE_SOURCE_INPUTS})
+        
+        get_filename_component(FILE_NAME "${SOURCE_INPUT}" NAME)
+        get_filename_component(FILE_DIR  "${SOURCE_INPUT}" DIRECTORY)
+        set(OUTPUT_FILENAME "${PACKAGE_SOURCE_BASE_OUTPUT_DIR}/${FILE_NAME}")
+        find_file(BINARY_FILE_FOUND ${FILE_NAME} PATHS ${PACKAGE_SOURCE_BASE_OUTPUT_DIR} NO_DEFAULT_PATH)
+
+        if((NOT BINARY_FILE_FOUND) OR PACKAGE_FORCE_DOWNLOAD)
+		    file(DOWNLOAD ${SOURCE_INPUT} ${OUTPUT_FILENAME} SHOW_PROGRESS)
+		    message(STATUS "Downloaded ${SOURCE_INPUT} to ${OUTPUT_FILENAME}")
+        endif()
+	
+    endforeach()
+
+endmacro()
--- source_downloader.md
+++ source_downloader.md
@@ -10,6 +10,14 @@ Args:
 - COMMIT_VALUE: Optional, referred to the Commit to point, if not set, points to the head/main of the of GITHUB, is HIGHLY ADVISABLE to set this variable. If you want to point to the head of a branch, set it as `refs/heads/<branch_name>` 
 - FORCE_DOWNLOAD: Optional, used to force the download, even if the file is found
 
+`download_remote_files(<SOURCE_BASE_OUTPUT_DIR> SOURCE_INPUTS... [FORCE_DOWNLOAD])`:
+this function is used to search for a package, if it is found the macro is aborted, but if it was not found, the macro will call an install script to clone the dependency and install it.
+
+Args:
+- SOURCE_BASE_OUTPUT_DIR: Referred to the output directory location.
+- SOURCE_INPUTS: Referred to the list of files to be downloaded, now with FULL URL, they will be saved in the SOURCE_BASE_OUTPUT_DIR.
+- FORCE_DOWNLOAD: Optional, used to force the download, even if the file is found
+
 ## Tips for source base url:
 
 Here are some patterns to set the value SOURCE_BASE_URL:
