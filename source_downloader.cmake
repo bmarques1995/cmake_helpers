de95ca7e64fca18444a5cb3833d93261bef6b698
bmarques1995 <testebash@outlook.com>
2025-03-03 09:44:43 -0300

    Unset find_file var and limited search path

--- source_downloader.cmake
+++ source_downloader.cmake
@@ -21,22 +21,23 @@ macro(download_sources_from_git_repo)
 
     foreach(SOURCE_INPUT ${PACKAGE_SOURCE_INPUTS})
         
-        set(OUTPUT_FILENAME "${PACKAGE_SOURCE_BASE_OUTPUT_DIR}/${SOURCE_INPUT}")
+		set(OUTPUT_FILENAME "${PACKAGE_SOURCE_BASE_OUTPUT_DIR}/${SOURCE_INPUT}")
         
         get_filename_component(FILE_NAME "${SOURCE_INPUT}" NAME)   # "my_header.h"
         get_filename_component(FILE_DIR  "${SOURCE_INPUT}" DIRECTORY)  # "include"
         if(${FILE_DIR})
-            find_file(SOURCE_FILE_FOUND ${FILE_NAME} PATHS ${PACKAGE_SOURCE_BASE_OUTPUT_DIR}/${FILE_DIR} )
+            find_file(SOURCE_FILE_FOUND ${FILE_NAME} PATHS ${PACKAGE_SOURCE_BASE_OUTPUT_DIR}/${FILE_DIR} NO_DEFAULT_PATH)
         else()
-            find_file(SOURCE_FILE_FOUND ${FILE_NAME} PATHS ${PACKAGE_SOURCE_BASE_OUTPUT_DIR})
+            find_file(SOURCE_FILE_FOUND ${FILE_NAME} PATHS ${PACKAGE_SOURCE_BASE_OUTPUT_DIR} NO_DEFAULT_PATH)
         endif()
 
         if((NOT SOURCE_FILE_FOUND) OR PACKAGE_FORCE_DOWNLOAD)
             set(INPUT_FILENAME "${PACKAGE_SOURCE_BASE_URL}/${PACKAGE_COMMIT_VALUE}/${SOURCE_INPUT}")
 		    file(DOWNLOAD ${INPUT_FILENAME} ${OUTPUT_FILENAME} SHOW_PROGRESS)
-		    message(STATUS "Downloaded ${SOURCE_INPUT} to ${OUTPUT_FILE}")
+            message(STATUS "Downloaded ${SOURCE_INPUT} to ${OUTPUT_FILE}")
         endif()
 	
+        unset(SOURCE_FILE_FOUND CACHE)
     endforeach()
 
 endmacro()
